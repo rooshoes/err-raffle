a4d71cbf128bb6c751726948b1e85182bce570ea
Jeston Tigchon <Jeston.Tigchon@bcgdv.com>
2016-06-26 02:08:24 -0700

    Raffle picker logic and templates.

--- raffle.py
+++ raffle.py
@@ -1,27 +1,55 @@
-
 import random
 from errbot import BotPlugin, botcmd
+from errbot.templating import tenv
+from errbot.core_plugins.acls import glob
+from time import sleep
 
+import logging
+log = logging.getLogger(__name__)
 
 class Raffle(BotPlugin):
-    """A simple plugin that returns random numbers"""
+    """A plugin to pick a random user from the userlist"""
     min_err_version = '1.6.0'
     max_err_version = '2.1.0'
 
-    @botcmd(split_args_with=None)
-    def raffle(self, mess, args):
-        """Get a random number between 1-100.
-        Or Specify the range. [min]-[max] If only one arg is given its
-        assumed to be max and min is set to 1"""
+    def get_configuration_template(self):
+        """Defines the configuration structure this plugin supports
+        You should delete it if your plugin doesn't use any configuration like this"""
+        return {'PREFIX': "WINNER:", 'IGNORED': ['*' + self.bot_config.CHATROOM_FN + '*'] + list(self.bot_config.BOT_ADMINS) }
+
+    def configure(self, config):
+        if config:
+            if type(config) != dict:
+                raise Exception('Wrong configuration type')
+
+            if not 'PREFIX' in config:
+                raise Exception('Wrong configuration type, it should contain PREFIX')
+
+        super(Raffle, self).configure(config)
+
+    @botcmd
+    def spin(self, msg, args):      
+        if not glob(msg.frm, self.bot_config.BOT_ADMINS):
+            return
+
         try:
-            if not args:
-                a, b = 1, 100
-            elif len(args) == 1:
-                a, b = 1, int(args[0])
-            elif len(args) == 2:
-                a, b = int(args[0]), int(args[1])
-            else:
-                raise ValueError
-        except ValueError:
-            return "Sorry dude, i can only handle two arguments. Both must be numbers!"
-        return u'{0} ({1}, {2})'.format(random.randint(a, b), a, b)
+            room = msg.frm.room
+        except AttributeError:
+            room = self._bot.rooms()[0]
+
+        viewers = [x for x in room.occupants if not glob(x.person, self.config['IGNORED'])]
+        if not viewers:
+            return "sorry, pool's empty."
+
+        winner = random.choice(viewers).nick
+        prefix_text = self.config['PREFIX']
+
+        dest = room if msg.is_group else msg.frm
+        self.send(dest, tenv().get_template('prefix.html').render( viewers=viewers ))
+        if not (args == 'quick'):
+            sleep(1)
+            for i in range(5):
+                self.send(dest, ' :catface: ' * i)
+                sleep(1)
+            sleep(2)
+        self.send(dest, tenv().get_template('winner.html').render( prefix=prefix_text, winner=winner ))
